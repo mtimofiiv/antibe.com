49f0ce2f11cfb450c1e6db99d83db9b549698216
JoeMoe <joemoe1984@gmail.com>
2015-05-06 01:45:27 -0700

    Renamed the header-new file to header-home edited the homepage and header files tweaked responsiveness on homepage

---
+++ wp-content/themes/sparkling/templates/home-template.php
@@ -0,0 +1,189 @@
+<?php 
+/**
+ * Template Name: Homepage
+ */
+get_header('home');
+?>
+
+<div class="section-home">
+	<div class="container">
+		<a name="home" id="home"></a>
+		<h1>Antibe Resources Ltd</h1>
+	</div>
+
+	<div class="tagline">
+		<div class="container">
+			<p>We are a multi-service consulting agency made up of a group of highly experienced professionals, here to advise on and implement on your most complex projects.</p>
+		</div>
+	</div>
+
+</div>
+<div class="section-team">
+	<a name="team" id="team"></a>
+	<div class="section-marker">
+		<div class="container">
+			<h2 class="section-title">
+				Dave &amp; the Team
+			</h2>
+		</div>
+	</div>
+	<div class="container">
+		<p class="announce">We are a network of highly experienced professionals covering a wide-range of roles, who are matched to each project based on its needs. Our primary team is built from a group of people who have many years of previous experience working together on diverse projects and who all believe in delivering excellence through collaboration and trust.</p>
+	</div>
+	<div class="container">
+		<div class="profile-img-wrapper align-center-xs col-md-2 col-sm-3">
+			<div class="profile-img center-block">
+				<img class="img-responsive" src="<?php echo get_template_directory_uri() . '/img/dave.jpg'; ?>" alt="">
+			</div>
+		</div>
+		<div class="col-md-10 col-sm-9">
+			<h4 class="heading-small large-text-xs align-left-sm align-center-xs">
+				Dave Rogers, <br class="hide-sm"><span class="grey-xs red-sm">President</span>
+			</h4>
+			<p class="align-left-sm align-center-xs">
+				Dave has over 30 years of professional engineering and consulting experience spanning a wide range of industries, including Public Sector, Health, Education, Energy Resources and Travel, with companies such as Coopers &amp; Lybrand, PricewaterhouseCoopers and IBM. His focus on quality assurance, delivery excellence and global delivery have earned him the reputation as a trusted Executive Advisor for both service providers and their clients. Through Antibe Resources Ltd he is seeking to continue working on his passion of large-scale project delivery by building a network of trusted and experienced colleagues whom he can support in their career and personal growth.
+			</p>
+		</div>
+	</div>
+	<div class="container">
+		<div class="personel">
+			<!-- <div class="dave">
+				<div class="img-brace bubble-big" style="background: url('assets/dave.jpg'); background-position: top center; background-size: cover;"></div>
+				<h3>Dave Rogers <br><small>President</small></h3>
+				<p>Dave has over 30 years of professional engineering and consulting experience spanning a wide range of industries, including Public Sector, Health, Education, Energy Resources and Travel, with companies such as Coopers &amp; Lybrand, PricewaterhouseCoopers and IBM. His focus on quality assurance, delivery excellence and global delivery have earned him the reputation as a trusted Executive Advisor for both service providers and their clients. Through Antibe Resources Ltd he is seeking to continue working on his passion of large-scale project delivery by building a network of trusted and experienced colleagues whom he can support in their career and personal growth.</p>
+			</div> -->
+			<div class="resources">
+				<ul>
+					<li>
+						<div class="img-brace" style="background: url('assets/incognito.jpg'); background-position: top center; background-size: cover;"></div>
+						<h4>Project Managers</h4>
+						<p>Our Project Management depth is significant and crosses many individuals and varying levels of scale and complexity. From Project Management of Technical Infrastructure and Data Centre migrations to Program Management of pan-Canadian Application Implementations, our senior resources bring many years of successful experience.</p>
+					</li>
+
+					<li>
+						<div class="img-brace" style="background: url('assets/incognito.jpg'); background-position: top center; background-size: cover;"></div>
+						<h4>Architects</h4>
+						<p>Our architect capabilities are focused in several very strong and experienced Architects who have very successfully performed as Enterprise, Application, Solution and Technical Architects. Their many years of experience is often sought in Technical Advisory roles.</p>
+					</li>
+
+					<li>
+						<div class="img-brace" style="background: url('assets/incognito.jpg'); background-position: top center; background-size: cover;"></div>
+						<h4>Business Analysts</h4>
+						<p>Our business Analysts capture and document business requirements through the use of planning sessions, business models, system surveys, etc while also understanding technical systems design, system development and maintenance, package implementation, testing and training.</p>
+					</li>
+
+					<li>
+						<div class="img-brace" style="background: url('assets/incognito.jpg'); background-position: top center; background-size: cover;"></div>
+						<h4>Consultants</h4>
+						<p>Our Consultants include Technical and Systems Analysts as well as Developers across several technologies. Some areas of expertise include ASD/ Outsourcing, Business and Market Analysis, Business Process Improvement, and Organizational Design. Technical focus includes Java and Oracle technologies and applications.</p>
+					</li>
+
+					<li>
+						<div class="img-brace" style="background: url('assets/incognito.jpg'); background-position: top center; background-size: cover;"></div>
+						<h4>Specialists</h4>
+						<p>Many of our practitioners have gained the knowledge and experience to be considered an expert or specialist in their field, either industry focused, such as in Healthcare or LNG/Oil &amp; Gas, or skills focused, such asin  Business Intelligence or Benefits Realization. These resources function as Consultants, Project Leaders, Analysts or even Advisors in the delivery of their specialty skills.</p>
+					</li>
+
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+
+	</div>
+</div>
+
+<div class="section-services">
+	<a name="services" id="services"></a>
+	<h2 class="section-marker"><div class="contain">Services</div></h2>
+	<div class="contain">
+		<p class="announce-big">Antibe is a group of experienced resources specializing in</p>
+
+		<ul class="services-array">
+			<li>
+				<h3 class="bebas">Strategy &amp; Change Implementation</h3>
+				<div class="v-keyline"></div>
+				<div class="graphic">&#xf085;</div>
+				<div class="v-keyline"></div>
+				<p>We work with organizations to develop a strategy to achieve their goals and focus and define their needs for required projects. We specialize in organizational transformation, process improvement and change management and communications.</p>
+			</li>
+			<li>
+				<h3>Full life cycle project delivery</h3>
+				<div class="v-keyline"></div>
+				<div class="graphic">&#xf0e8;</div>
+				<div class="v-keyline"></div>
+				<p>From concept to delivery, we produce high quality work at every step of the project life cycle, including planning, development, testing and deployment.</p>
+			</li>
+			<li>
+				<h3>Package &amp; systems implementation</h3>
+				<div class="v-keyline"></div>
+				<div class="graphic">&#xf12e;</div>
+				<div class="v-keyline"></div>
+				<p>We work with you to determine the best option for your needs and develop or configure the right software system. Our experienced team of architects, specialists and developers ensure quality customized systems.</p>
+			</li>
+			<li>
+				<h3>Quality Assurance &amp; Testing</h3>
+				<div class="v-keyline"></div>
+				<div class="graphic">&#xf0e7;</div>
+				<div class="v-keyline"></div>
+				<p>With a focus on quality assurance we recognize and ensure that appropriate testing and reviews are conducted at all stages of a project lifecycle.</p>
+			</li>
+		</ul>
+		<div class="clear"></div>
+	</div>
+</div>
+
+<div class="section-join">
+	<a name="join" id="join"></a>
+	<h2 class="section-marker"><div class="contain">Join Us</div></h2>
+	<div class="contain">
+		<p class="announce">Our vision is not only to deliver excellence to our clients, but also to provide an environment for skilled, experienced professionals to work together in a flexible environment on large-scale, exciting projects.</p>
+
+		<ul class="two-column">
+
+			<li>
+				<h3>What do we have to offer you?</h3>
+
+				<div class="graphic">&#xf084;</div>
+
+				<p>We seek opportunities to work for large service providers or client organizations</p>
+
+				<p>We create a group of people who are willing to help and support each other in a flexible work environment</p>
+
+				<p>We provide a reasonable revenue to practitioners while working on challenging yet enjoyable projects</p>
+			</li>
+
+			<li>
+				<h3>Why we are different</h3>
+
+				<div class="graphic">&#xf0c0;</div>
+
+				<p>We trust each other not only to deliver in a professional manner but also trust each other to ask for help or advice when needed</p>
+				<p>We seek opportunities that allow individuals to meet their desired work/life balance</p>
+				<p>We are passionate and bring a level of team spirit to every engagement</p>
+			</li>
+
+		</ul>
+
+		<div class="clear"></div>
+
+		<p class="announce">We are always interested in meeting like-minded, experienced professionals who are looking to do project-based work as part of a passionate and skilled team. We believe in collaboration and excellence as part of every project.</p>
+	</div>
+</div>
+
+<div class="section-contact">
+	<a name="contact" id="contact"></a>
+	<h2 class="section-marker"><div class="contain">Contact Us</div></h2>
+	<div class="contain">
+		<p class="announce-big">If you are a a service provider or client organization looking for project delivery support, or if you are a contractor looking for exciting work opportunities, please contact us at:</p>
+
+		<a href="mailto:info@antibe.com" class="big-link-btn"><i>&#xf003;</i> info@antibe.com</a>
+		<a href="http://www.linkedin.com/company/antibe-resources-ltd-" class="big-link-btn"><i>&#xf0e1;</i> /company/antibe-resources-ltd</a>
+		<a href="https://plus.google.com/117473405488972704721" class="big-link-btn" rel="author"><i>&#xf0d5;</i> Google +</a>
+
+		<div style="height: 250px;"></div>
+
+	</div>
+</div>
+
+
+<?php get_footer('home'); ?>
